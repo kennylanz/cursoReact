5b18c5d7449b3dd2b6c4ac272d82c4e54b92fb82
kennylanz <68557484+kennylanz@users.noreply.github.com>
2021-03-17 20:51:50 -0300

    acá esta el menu del desafío 2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/NavBar';
 
 
-function  Navbar() {
-  return <> 
-          <h2>Música electrónica</h2>
-          <CartWidget />
-          </>;
-}
-function ItemListContainer() {
-}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+    <NavBar />
+    <h2>Listo</h2> 
     </div>
+    
+
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const NavBar = () => {
+    return (
+        <div>
+          <h3>La tienda</h3>
+          <ul>
+            <li><a href="">Vídeos</a></li>
+            <li><a href="">Solo Audio</a></li>
+            <li><a href="">Fotos</a></li>
+          </ul>
+        </div>
+    )
+}
